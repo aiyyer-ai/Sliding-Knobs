944449002ff45f771a7ce2fcac3694e1be28e1a3
aiyyer <mitchrc2000@gmail.com>
2024-03-14 16:06:59 -0700

    implemented RandomSeed

--- script.js
+++ script.js
@@ -9,13 +9,53 @@ var allInputs = document.getElementsByClassName('inputs');
 Array.from(allInputs).forEach(function(singleInput){
   singleInput.addEventListener("keyup", function(event) {
     // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13 || singleInput.value.length == 1) {
+    if (event.keyCode === 13) {
       // Focus on the next sibling
       singleInput.nextElementSibling.focus();
       singleInput.nextElementSibling.select();
     }
   });
 })
+
+function cyrb128(str) {
+    let h1 = 1779033703, h2 = 3144134277,
+        h3 = 1013904242, h4 = 2773480762;
+    for (let i = 0, k; i < str.length; i++) {
+        k = str.charCodeAt(i);
+        h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+        h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+        h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+        h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+    }
+    h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
+    h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
+    h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
+    h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
+    h1 ^= (h2 ^ h3 ^ h4), h2 ^= h1, h3 ^= h1, h4 ^= h1;
+    return [h1>>>0, h2>>>0, h3>>>0, h4>>>0];
+}
+
+function sfc32(a, b, c, d) {
+    return function() {
+      a |= 0; b |= 0; c |= 0; d |= 0; 
+      var t = (a + b | 0) + d | 0;
+      d = d + 1 | 0;
+      a = b ^ b >>> 9;
+      b = c + (c << 3) | 0;
+      c = (c << 21 | c >>> 11);
+      c = c + t | 0;
+      return (t >>> 0) / 4294967296;
+    }
+}
+
+function testRandom() {
+	let seedString = document.getElementById('gameSeed').value;
+	var seedHash = cyrb128(seedString);
+	var rand = sfc32(seedHash[0], seedHash[1], seedHash[2], seedHash[3]);
+	console.log(rand());
+	console.log(rand());
+	console.log(rand());
+}
 
 function populateGrid() {
 	let gridWidth = document.getElementById('gridWidth').value;
